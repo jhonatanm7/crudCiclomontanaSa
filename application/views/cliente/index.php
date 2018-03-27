8815c9339b58f59c6abefdd90a705572647cdb37
Yonatan Montoya <jhonatanm7@gmail.com>
2018-03-27 11:00:04 -0500

    Error de nombre carpeta
    Se corrige error nombre de carpeta assets el cual creaba un conflicto a la hora de hacer llamado de libreria.

--- application/views/cliente/index.php
+++ application/views/cliente/index.php
@@ -5,18 +5,20 @@
 </head>
 <body>
 	<h1>Hola Cliente Bienvenido</h1>
+	<div id="PersonTableContainer"></div>
+
+
+
 
 
 	<!--llamado a las librerias de Jquery y jtable -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url() ?>/asstes/plugins/jquery-ui-1.12.1.custom/jquery-ui-min.css">
-	<script src="<?php echo base_url() ?>/asstes/plugins/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-	<!-- Include one of jTable styles. -->
-	<link href="<?php echo base_url() ?>/asstes/plugins/jtable.2.4.0/themes/metro/blue/jtable.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1.custom/jquery-ui-min.css">	
+	<script src="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+	<link href="<?php echo base_url() ?>assets/plugins/jtable.2.4.0/themes/metro/blue/jtable.min.css" rel="stylesheet" type="text/css" />
 	 
 	<!-- Include jTable script file. -->
-	<script src="<?php echo base_url() ?>/asstes/plugins/jtable.2.4.0/jquery.jtable.min.js" type="text/javascript"></script>
-
+	<script src="<?php echo base_url() ?>assets/plugins/jtable.2.4.0/jquery.jtable.min.js" type="text/javascript"></script>
 	<script type="text/javascript">
     $(document).ready(function () {
         $('#PersonTableContainer').jtable({
