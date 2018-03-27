2bb2fbfb2da3071d5a2dd3a513b7c9381b1b4bc9
Yonatan Montoya <jhonatanm7@gmail.com>
2018-03-27 11:30:34 -0500

    Configuracion Jtable idioma

--- application/views/cliente/index.php
+++ application/views/cliente/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Visitas CLientes</title>
 </head>
 <body>
 	<h1>Hola Cliente Bienvenido</h1>
@@ -13,16 +13,18 @@
 
 	<!--llamado a las librerias de Jquery y jtable -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1.custom/jquery-ui-min.css">	
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1.custom/jquery-ui.min.css">	
 	<script src="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
 	<link href="<?php echo base_url() ?>assets/plugins/jtable.2.4.0/themes/metro/blue/jtable.min.css" rel="stylesheet" type="text/css" />
 	 
 	<!-- Include jTable script file. -->
+	
 	<script src="<?php echo base_url() ?>assets/plugins/jtable.2.4.0/jquery.jtable.min.js" type="text/javascript"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/jtable.2.4.0/localization/jquery.jtable.es.js" type="text/javascript"></script>
 	<script type="text/javascript">
     $(document).ready(function () {
         $('#PersonTableContainer').jtable({
-            title: 'Table of people',
+            title: 'Lista de clientes',
             actions: {
                 listAction: '/GettingStarted/PersonList',
                 createAction: '/GettingStarted/CreatePerson',
@@ -30,20 +32,20 @@
                 deleteAction: '/GettingStarted/DeletePerson'
             },
             fields: {
-                PersonId: {
+                nitCliente: {
                     key: true,
                     list: false
                 },
-                Name: {
-                    title: 'Author Name',
+                nomClienteCom: {
+                    title: 'Nombre Completo',
                     width: '40%'
                 },
-                Age: {
-                    title: 'Age',
+                dirCliente: {
+                    title: 'Direccion ',
                     width: '20%'
                 },
-                RecordDate: {
-                    title: 'Record date',
+                telCliente: {
+                    title: 'Telefono',
                     width: '30%',
                     type: 'date',
                     create: false,
